907f5ed7203b4f451fa9ef409f99b16b03789a64
mtvs <mtvsdev@gmail.com>
2022-04-20 02:57:52 +0430

    Remove a todo item

--- src/Reviewable.php
+++ src/Reviewable.php
@@ -13,13 +13,11 @@ trait Reviewable
 	{
 		$reviewClass = app('review_class');
 
-		// todo: get the rating column from the review class
 		$totalAverage = $reviewClass::where('reviewable_type', get_class($this))
 		->avg('rating');
 		$averageCount = $reviewClass::where('reviewable_type', get_class($this))
 		->count() / $this->count();
 
-		// todo: get the rating column from the review class
 		$query->withAvg('reviews as itemAverage', 'rating')
 			->withCount('reviews as itemCount')
 			->orderByRaw('(IFNULL(itemAverage, 0) * itemCount + ? * ?) / (itemCount + ?) DESC', [
@@ -29,7 +27,6 @@ trait Reviewable
 
 	public function scopeWithRatings($query)
 	{
-		// todo: get the rating column from the review class
 		$query->withAvg('reviews as rating_avg', 'rating')
 			->withCount('reviews as rating_count');
 	}

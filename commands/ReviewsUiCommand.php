13a9f62a70f92bcca6805c714125e671e2ca8af9
mtvs <mtvs.dev@gmail.com>
2022-07-25 10:46:42 +0430

    Fix the ui command

--- commands/ReviewsUiCommand.php
+++ commands/ReviewsUiCommand.php
@@ -21,10 +21,19 @@ class ReviewsUiCommand extends Command
             resource_path('js/components/reviews')
         );
 
+
+        if (! $filesystem->isDirectory(public_path('css'))) {
+            $filesystem->makeDirectory(public_path('css'));
+        }
+
         $filesystem->copy(
             __DIR__.'/../stubs/resources/css/reviews.css',
             public_path('css/reviews.css')
         );
+
+        if (! $filesystem->isDirectory(public_path('font'))) {
+            $filesystem->makeDirectory(public_path('font'));
+        }        
 
         $filesystem->copyDirectory(
             __DIR__.'/../stubs/resources/font',

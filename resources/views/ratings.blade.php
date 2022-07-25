9c4eb44560a314de2cbcc86e7d41d4033efeefc9
mtvs <mtvs.dev@gmail.com>
2022-07-25 18:48:20 +0430

    Show ratings only when there is at least one

--- resources/views/ratings.blade.php
+++ resources/views/ratings.blade.php
@@ -1,34 +1,36 @@
-<span>
-	<span class="rating-stars d-inline-flex">
-		@foreach(range(1, $max()) as $n)
-			<i class="{{ $starIcon($n) }}"></i>
-		@endforeach
-	</span>
-
+@if($count)
 	<span>
-		{{ sprintf("(%.1f from %d reviews)", $average, $count) }}
-	</span>
-</span>
-
-@if($ratios)
-	<div class="card">
-		<div class="card-body">
-			@foreach($ratios as $rating => $ratio)
-				<div class="d-flex mb-1">
-					<div class="w-25">
-						<span class="badge bg-primary">
-							{{ $rating }} 
-							<i class="icon-star"></i>
-						</span>
-					</div>
-
-					<div class="progress w-75">
-						<div class="progress-bar" style="width: {{ $ratio }}%;">
-							{{ $ratio }}%
-						</div>
-					</div>			
-				</div>
+		<span class="rating-stars d-inline-flex">
+			@foreach(range(1, $max()) as $n)
+				<i class="{{ $starIcon($n) }}"></i>
 			@endforeach
+		</span>
+
+		<span>
+			{{ sprintf("(%.1f from %d reviews)", $average, $count) }}
+		</span>
+	</span>
+
+	@if($ratios)
+		<div class="card">
+			<div class="card-body">
+				@foreach($ratios as $rating => $ratio)
+					<div class="d-flex mb-1">
+						<div class="w-25">
+							<span class="badge bg-primary">
+								{{ $rating }} 
+								<i class="icon-star"></i>
+							</span>
+						</div>
+
+						<div class="progress w-75">
+							<div class="progress-bar" style="width: {{ $ratio }}%;">
+								{{ $ratio }}%
+							</div>
+						</div>			
+					</div>
+				@endforeach
+			</div>
 		</div>
-	</div>
+	@endif
 @endif

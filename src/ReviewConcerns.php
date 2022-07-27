2cdb4088a32fbcf7b566238387c6a3a8b4e33b0e
mtvs <mtvs.dev@gmail.com>
2022-07-27 12:27:27 +0430

    Add the phpdoc for the ReviewConcerns

--- src/ReviewConcerns.php
+++ src/ReviewConcerns.php
@@ -3,22 +3,39 @@
 namespace Mtvs\Reviews;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Mtvs\EloquentApproval\Approvable;
 
 trait ReviewConcerns
 {
 	use Approvable;
 
+	/**
+	 * Get the relation to the user model
+	 *
+	 * @return BelongsTo
+	 **/
 	public function user()
 	{
 		return $this->belongsTo(config('auth.providers.users.model'));
 	}
 
+	/**
+	 * Get the relation to the reviewable model
+	 * 
+	 * @return MorphTo
+	 **/
 	public function reviewable()
 	{
 		return $this->morphTo();
 	}
 
+	/**
+	 * Add the query scope for the reviewable model
+	 * 
+	 * @return void
+	 **/
 	public function scopeReviewable($query, Model $reviewable)
 	{
 		$query->whereMorphedTo($this->reviewable(), $reviewable);
